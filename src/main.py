1bb236c34015e69c0df2e0bcd4188965eafcc15a
Michael Uhlenberg <michael.uhlenberg@t-online.de>
2022-10-28 19:00:57 +0200

    map user to (path, rights)

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 import google.auth.transport.requests
 from google.oauth2.credentials import Credentials
@@ -17,6 +18,8 @@ SCOPES = [
 
 class GDrivePerms:
     def __init__(self):
+        self.mapP2UR = {}   # map path to [(user, right)]
+        self.mapU2PR = {}   # map user to [(path, right)]
         creds = None
         # The file token.json stores the user's access and refresh tokens, and is
         # created automatically when the authorization flow completes for the first
@@ -39,6 +42,22 @@ class GDrivePerms:
             self.service = googleapiclient.discovery.build('drive', 'v3', credentials=creds)
         except HttpError as error:
             print(f'An error occurred: {error}')
+
+    def mapAdd(self, path, type, role, email):
+        # map path to (user,rights)
+        pathEntry = self.mapP2UR.get(path)
+        if pathEntry is None:
+            pathEntry = []
+            self.mapP2UR[path] = pathEntry
+        pathEntry.append((type, role, email))
+        # map user to (path, rights)
+        if email is None:
+            email = "_" + type
+        pathEntry = self.mapU2PR.get(email)
+        if pathEntry is None:
+            pathEntry = []
+            self.mapU2PR[email] = pathEntry
+        pathEntry.append((type, role, path))
 
     def listDrives(self):
         nextPageTokenD = None
@@ -96,7 +115,7 @@ class GDrivePerms:
                 break
         return res
 
-    def listPerms(self, fileId, indent):
+    def listPerms(self, fileId, indent, path):
         lines = []
         permList = self.service.permissions().list(
             fileId=fileId,
@@ -106,7 +125,7 @@ class GDrivePerms:
         for perm in perms:
             p = self.service.permissions().get(
                 fileId=fileId, permissionId=perm["id"],
-                fields='*',
+                fields='type,role,permissionDetails,emailAddress,allowFileDiscovery',
                 supportsAllDrives=True).execute()
             assert p["type"] == perm["type"]
             assert p["role"] == perm["role"]
@@ -124,11 +143,13 @@ class GDrivePerms:
 
             type = p["type"]
             role = p["role"]
+            email = p.get('emailAddress')
+            self.mapAdd(path, type, role, email)
             indentS = str(" " * indent)
             if type == "user":
-                lines.append("User " + p['emailAddress'] + " " + role)
+                lines.append("User " + email + " " + role)
             elif type == "group":
-                lines.append("Group " + p['emailAddress'] + " " + role)
+                lines.append("Group " + email + " " + role)
             elif type == "domain":
                 lines.append("ADFCM " + role + (" allow discovery" if p["allowFileDiscovery"] else ""))
             elif type == "anyone":
@@ -137,19 +158,57 @@ class GDrivePerms:
                 lines.append("TODO " + type + " " + role)
         return "\n".join([str(" " * indent) + line for line in lines])
 
-    def listFiles(self, files, indent):
+    def listFiles(self, files, indent, path):
         for file in files:
             try:
-                output = self.listPerms(file["id"], indent + 3)
+                output = self.listPerms(file["id"], indent + 3, path + "/" + file["name"])
                 if output != "":
                     print(str(" " * indent) + f"{file['name']} {file['mimeType']}")
                     print(output)
                 if file["mimeType"] == "application/vnd.google-apps.folder":
                     subFiles = self.listFilesInDir(file["id"])
-                    self.listFiles(subFiles, indent + 3)
+                    self.listFiles(subFiles, indent + 3, path + "/" + file["name"])
             except Exception as e:
                 print("Error", e)
 
+    def printU2PR(self):
+        users = sorted(self.mapU2PR.keys())
+        for user in users:
+            print(user)
+            for pr in self.mapU2PR[user]:
+                print("  ", pr[2], pr[0], pr[1])  # path, type, role
+
+
+def main():
+    gdp = GDrivePerms()
+
+    print("My Files")
+    files = gdp.listRootLevelFiles(None)
+    gdp.listFiles(files, 0, "MyDrive")
+
+    print()
+    print()
+    print('Geteilte Ablagen:')
+    drives = gdp.listDrives()
+    for drive in drives:
+        print()
+        drvName = drive["name"]
+        driveId = drive["id"]
+        print(drvName)
+        print(gdp.listPerms(driveId, 3, drvName))
+        print()
+        files = gdp.listRootLevelFiles(driveId)
+        gdp.listFiles(files, 3, drvName)
+    print()
+    with open("p2ur.json", "w") as fp:
+        json.dump(gdp.mapP2UR, fp, indent=2)
+    with open("u2pr.json", "w") as fp:
+        json.dump(gdp.mapU2PR, fp, indent=2)
+    print("Users to Path/Rights:")
+    gdp.printU2PR()
+
+if __name__ == '__main__':
+    main()
 
 """
 Alle:
@@ -170,29 +229,3 @@ group   fileOrganizer
 group   organizer
 user    fileOrganizer
 """
-
-
-
-def main():
-    gdp = GDrivePerms()
-
-    print("My Files")
-    files = gdp.listRootLevelFiles(None)
-    gdp.listFiles(files, 0)
-
-    print()
-    print()
-    print('Geteilte Ablagen:')
-    drives = gdp.listDrives()
-    for drive in drives:
-        print()
-        print(drive["name"])
-        driveId = drive["id"]
-        print(gdp.listPerms(driveId, 3))
-        print()
-        files = gdp.listRootLevelFiles(driveId)
-        gdp.listFiles(files, 3)
-    print()
-
-if __name__ == '__main__':
-    main()
